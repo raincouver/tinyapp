849071fefda533aafbf56c0bf1f8bbdf860c0c48
Rain Zhu <rain1995zhu@gmail.com>
2023-03-21 02:50:23 -0700

    Add GET route to expressserver.js and render using accompanying template; Add POST route to expressserver.js to receive form submission

--- express_server.js
+++ express_server.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
+
+function generateRandomString() {
+  
+}
+
+
+
+
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   'b2xVn2': "http://www.lighthouselabs.ca",
   '9sm5xK': "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send('Hello!');
@@ -22,10 +33,15 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL:urls[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
 
 // app.get('/hello', (req, res) =>{
 //   res.send('<html><body>Hello <b>World</b></body></html>\n');
@@ -34,6 +50,11 @@ app.get("/urls/:id", (req, res) => {
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
