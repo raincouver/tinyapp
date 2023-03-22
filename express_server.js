3fbb2623a50bef9d3e9a304a5287823a0688241b
Rain Zhu <rain1995zhu@gmail.com>
2023-03-22 01:53:42 -0700

    Modify _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
-
+const cookieParser = require('cookie-parser')
 
 function generateRandomString() {
   
@@ -15,10 +15,12 @@ const urlDatabase = {
   '9sm5xK': "http://www.google.com"
 };
 
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send('Hello!');
+  // res.send('Hello!');
+  res.redirect('/urls');
 });
 
 
@@ -27,17 +29,28 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  console.log(req);
-  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id] };
+  // console.log(req.cookies);
+  const templateVars = { 
+    id: req.params.id, 
+    longURL:urlDatabase[req.params.id],
+    username: req.cookies["username"]
+   };
   res.render("urls_show", templateVars);
 });
 
@@ -70,10 +83,10 @@ app.get("/u/:id", (req, res) => {
 
 
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: "Hello World!" };
+//   res.render("hello_world", templateVars);
+// });
 
 // app.post("/urls", (req, res) => {
 //   console.log(req.body); // Log the POST request body to the console
