b7b01a6658ca3cf03aee28dc868831777c8bc18d
Rain Zhu <rain1995zhu@gmail.com>
2023-03-22 18:25:07 -0700

    Update _header.ejs to show email instead of username; and improved some graphics for login and register pages

--- express_server.js
+++ express_server.js
@@ -4,18 +4,25 @@ const PORT = 8080; //default port 8080
 const cookieParser = require('cookie-parser')
 const morgan = require('morgan');
 
-function generateRandomString() {
-  
+function generateRandomString(length) {
+  const charRange ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  let num = 0;
+  while (num < length) {
+    result += charRange .charAt(Math.floor(Math.random() * charRange.length));
+    num += 1;
+  }
+  return result;
 }
 
 const users = {
-  '123': {
-    id:'123',
+  '00001': {
+    id:'00001',
     email:'a@a.com',
     password:'123'
   },  
-  '456': {
-    id:'456',
+  '00002': {
+    id:'00002',
     email:'b@b.com',
     password:'123'
   },
@@ -32,40 +39,54 @@ app.use(cookieParser()) // Parse Cookie populate req.cookies
 app.use(express.urlencoded({ extended: true })); //Parse Body populate req.body
 app.use(morgan('dev'))
 
+//landing page redirect to /urls
 app.get("/", (req, res) => {
   // res.send('Hello!');
   res.redirect('/urls');
 });
-
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get('/urls', (req, res) => {
+
   console.log(req.cookies);
+
+  let user_id = req.cookies["user_id"];
+
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[user_id]
   };
+
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+
+  let user_id = req.cookies["user_id"];
+
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[user_id]
   };
+
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
+
+  let user_id = req.cookies["user_id"];
+
   console.log(req.cookies);
+
   const templateVars = { 
     id: req.params.id, 
     longURL:urlDatabase[req.params.id],
-    username: req.cookies["username"]
+    user: users[user_id]
    };
+
   res.render("urls_show", templateVars);
 });
 
@@ -76,19 +97,65 @@ app.post("/urls/:id", (req, res) => {
 });
 
 // app.post("/logout", (req, res) => {
-//   res.cookie('username', "undefined", )
+//   res.cookie('user_id', "undefined", )
 //   res.redirect('/urls');
 // });
 
 // app.post("/login", (req, res) => {
-//   res.cookie('username', req.body.username, )
+//   res.cookie('user_id', req.body.user_id, )
 //   res.redirect('/urls');
 // });
 
 
-// GET /register
+// GET /Register
 app.get('/register', (req, res) => {
   res.render('registration');
+})
+
+// POST /Register
+app.post('/register', (req, res) => {
+  console.log(req.body)
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Check if email and password were not provided
+  if (!email) {
+    return res.status(400).send('Please provide an email address to continue!');
+  }
+  if (!password) {
+    return res.status(400).send('Please provide a password to continue!');
+  }
+
+  //Check if email is already registered
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      return res.status(400).send('Looks like you have already registered with us, log in with your password!');
+    }
+  }
+
+  //Assign new user with new id
+  // let newUserId = String(Object.keys(users).length + 1).padStart(5, '0'); 
+  let newUserId = generateRandomString(6);
+
+  //Check if the newly generated ID is already existed
+  for (const userId in users) {
+    if (newUserId === userId) {
+      //generate a new one and check again
+      newUserId = newUserId + email;
+    }
+  }
+
+  users[newUserId] = {
+    id:newUserId,
+    email:email,
+    password:password
+  };
+  
+  //The enetered credentials are correct
+  //Set a cookie and then redirect the user
+  res.cookie('user_id', newUserId);
+  res.redirect('/urls');
 })
 
 // GET /LOGIN
@@ -129,38 +196,45 @@ app.post('/login', (req, res) => {
 
   //The enetered credentials are correct
   //Set a cookie and then redirect the user
-  res.cookie('userId', foundUser.id);
-  res.redirect('/protected');
+  res.cookie('user_id', foundUser.id);
+  res.redirect('/');
 })
 
-//GET /PROTECTED
-app.get('/protected', (req, res) => {
-  //check if the user is logged in 
-  const userId = req.cookies.userId;
-  const user = users[userId];
+// //GET /PROTECTED
+// app.get('/protected', (req, res) => {
+//   //check if the user is logged in 
+//   const userId = req.cookies.userId;
+//   const user = users[userId];
 
-  if (!userId) {
-    return res.status(401).send('You must log in to see this page!');
-  }
+//   if (!userId) {
+//     return res.status(401).send('You must log in to see this page!');
+//   }
 
-  const templateVars = {
-    user: user,
-  }
+//   const templateVars = {
+//     user: user,
+//   }
 
-  console.log(user);
-  //Happy Path is render the protected template
-  res.render('protected', templateVars);
-})
+//   console.log(user);
+//   //Happy Path is render the protected template
+//   res.render('protected', templateVars);
+// })
 
 //POST /logout
 app.post('/logout', (req, res) => {
   //Clear the cookie
-  res.clearCookie('userId');
+  res.clearCookie('user_id');
 
   //redirect the user
   res.redirect('/login');
 })
+// //POST /logout
+// app.get('/logout', (req, res) => {
+//   //Clear the cookie
+//   res.clearCookie('user_id');
 
+//   //redirect the user
+//   res.redirect('/login');
+// })
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
